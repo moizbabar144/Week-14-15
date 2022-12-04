3f4fae62629acc1ba2ef99c70091f98c23546515
Your Name <you@example.com>
2022-12-04 07:30:14 -0800

    week14+15

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD9HC_U8QtVZM2C8svFs5yupPxDXwXXKJw",
+  authDomain: "crud-20ef9.firebaseapp.com",
+  projectId: "crud-20ef9",
+  storageBucket: "crud-20ef9.appspot.com",
+  messagingSenderId: "669205129291",
+  appId: "1:669205129291:web:0d9b74b8ceb1926171c5ce"
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const database = getFirestore();
+
+export { app, database }
